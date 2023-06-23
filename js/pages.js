ee3ca15fa567a26ce1eecb71560f71b6a253911d
wizli595 <abdoowizli@gmail.com>
2023-06-23 12:25:11 +0100

    con

--- js/pages.js
+++ js/pages.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+  $("#txt").keyup(function (e) {
+    $(".keys").empty();
+    let strt = $("#txt").val();
+    do {
+      if (strt == "") return;
+      const url1 = `https://api.themoviedb.org/3/search/keyword?query=${strt}&language=en-US&page=1`;
+      const options1 = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMTY3NmRlOWI0YTAzYTdlOGQ0ODZjNTY3MWQ2N2UyNCIsInN1YiI6IjY0NDU3NzQzYWQ4N2Y3MTc2YTcyNzRjYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vQ0wTEOjweUUodHiy9LNTnkC0iwgnWzKofNhCbJ3R1M",
+        },
+      };
+      getKey(url1, options1);
+    } while (false);
+  });
   $("#btn").click(() => {
     $(".info").empty();
     let inp = $("#txt").val().replace(" ", "%20");
@@ -56,4 +73,26 @@ async function getApi(url, opt) {
       "<p id='errMsg'>WOW sorry not found try again or check what you typed ;)</p>";
     $(".info").append(errMsg);
   }
+}
+async function getKey(url, opt) {
+  const response = await fetch(url, opt);
+  let movie = await response.json();
+  let data = movie.results;
+  let errMsg = "";
+  if (data.length >= 1) {
+    data.forEach((e) => {
+      // makeCard(e);
+      $(".keys").append(
+        `<div class="key"><span onclick="fun(this)">${e.name}</span></div>`
+      );
+      // imgErr();
+    });
+  } else {
+    $(".keys").empty();
+    errMsg = "<p id='errMsg'>no suggestions</p>";
+    $(".keys").append(errMsg);
+  }
+}
+function fun(e) {
+  $("#txt").val(e.innerHTML);
 }
